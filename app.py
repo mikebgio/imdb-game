53bf8cc1ebbd9bed28bd7625cfa7ae4246cc8de6
Michael Giordano <mgiord1987@gmail.com>
2022-09-06 00:09:07 -0400

    adds better templating and more game structure to app and frontend

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
-
+import random
 from flask import Flask, render_template, request
 import game_host
+import uuid
 
 from pprint import pprint
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 host = game_host.GameHost(os.getenv('MONGO_URL'))
 
 
@@ -18,10 +19,22 @@ def home():
 def game():
     game_rounds = range(1, 6)
     game_round_num = game_rounds[0]
-    choices = host._get_three_movie_options()
+    choices = [choice for choice in host._get_three_movie_options()]
+    print(choices)
     return render_template('game.html',
                            game_round_num=game_round_num,
-                           choices=)
+                           choices=choices)
+
+
+@app.route('/movie/<movie_id>')
+def select_movie(movie_id):
+    movie = host.get_movie(movie_id)
+
+    return render_template('movie.html', current_category=cat_title,
+                           current_clue=current_clue['text'])
+
+
+def select_clue_from_movie(movie):
 
 
 if __name__ == "__main__":
