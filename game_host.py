a4b585c585b60da6b3279c5bde427fa527f6fafe
Michael Giordano <mgiord1987@gmail.com>
2022-09-18 22:36:26 -0400

    breaks database functionality out to a DB class, adds Player and Game classes with some placeholder methods, adds Todo list for game_host.py

--- game_host.py
+++ game_host.py
@@ -14,24 +14,35 @@ class GameHost:
     clues while also tracking the status of an individual game
     """
 
-    def __init__(self, mongo_url, player_id=None, game_instance=None):
-        self.player_id = player_id or str(uuid.uuid4())
-        self.game_instance = game_instance or str(uuid.uuid4())
-        self.game_inst_query = {'game_instance': self.game_instance,
-                           'player_id': self.player_id}
-        self.__client = MongoClient(mongo_url)
-        db = self.__client['imdb']
-        self.movie_db = db['movies']
-        self.game_db = db['games']
-        self.player_db = db['players']
+    def __init__(self, mongo_url: str = "mongodb://mongo:27017",
+                 player_id: str = None, game_instance=None):
+        self.mongo = DB(mongo_url)
+        this_player_id = player_id or str(uuid.uuid4())
+        self.player = Player(this_player_id)
+        this_game_id = game_instance or str(uuid.uuid4())
+        self.game = Game(this_game_id)
+        self.game_inst_query = {'game_instance': self.game.id,
+                                'player_id': self.player.id}
+
         self.played_movies = []
         self.rounds = []
+        self.current_round = None and self._set_current_round()
+        self._create_new_game()
+
+    def _set_player_data(self):
+        player_data = self.mongo.get_player(self.player.id)
+        self.player.set_player_data(player_data)
+
+    def _set_current_round(self):
+        self.current_round = len(self.rounds) + 1
 
     def _create_new_game(self):
-        self.game_db.insert_one(self.game_inst_query)
-        # self.player_db.update_one({'player_id': self.player_id, {'$push': {'games': self.game_instance}}})
+        self.mongo.games.insert_one(self.game_inst_query)
+        self.mongo.players.update_one({'player_id': self.player.id},
+                                      {'$push': {'games': self.game.id}})
 
     def _get_three_movie_options(self):
+        # TODO: This should probably move to DB class
         """
         Queries MongoDB for 3 random movies documents (doc_id, year)
         :return:
@@ -41,8 +52,8 @@ class GameHost:
             {'$match': {'_id': {'$nin': self.played_movies}}},
             {'$sample': {'size': 3}}
         ]
-        results = [choice for choice in self.movie_db.aggregate(query_pipeline)]
-        self.game_db.update_one(self.game_inst_query, {
+        results = [choice for choice in self.mongo.movies.aggregate(query_pipeline)]
+        self.mongo.games.update_one(self.game_inst_query, {
             '$push': {'rounds': [{'round': 1, 'choices': results}]}})
         return results
 
@@ -53,10 +64,11 @@ class GameHost:
         :return:
         """
         filter = {'_id': ObjectId(doc_id)}
-        movie = self.movie_db.find_one(filter)
-        self.game_db.update_one(self.game_inst_query, {
-            '$push': {'movies': {'id': movie['_id'], 'ts': datetime.utcnow()}}
-        })
+        movie = self.mongo.movies.find_one(filter)
+        game_update = {'$push': {'movies':
+                                     {'id': movie['_id'],
+                                      'ts': datetime.utcnow()}}}
+        self.mongo.games.update_one(self.game_inst_query, game_update)
         self.played_movies.append(movie['_id'])
         current_round = len(self.rounds) + 1
         self.rounds.append(
@@ -85,9 +97,109 @@ class GameHost:
             clue['points']: int = points
             points -= 1
 
+    def _register_guess(self, guess):
+        pass
 
+    def verify_answer(self, movie_obj: dict, guess: str):
+        true_answer = ''.join(
+            e for e in movie_obj['title'].lower() if e.isalnum())
+        player_answer = ''.join(
+            e for e in guess.lower() if e.isalnum())
+        print(f'{true_answer}\n{player_answer}')
+        if player_answer == true_answer:
+            return True
+        else:
+            return False
+
+
+class Player:
+    def __init__(self, player_id: str = None):
+        self.id = player_id or str(uuid.uuid4())
+
+    def set_player_data(self, data: dict):
+        self.info = data
+
+
+class Game:
+    def __init__(self, game_id: str = None):
+        self.id = game_id or str(uuid.uuid4())
+
+    def _is_game_new(self):
+        pass
+
+
+class DB:
+    def __init__(self, mongo_url: str = "mongodb://mongo:27017"):
+        client = MongoClient(mongo_url)
+        db = client['imdb']
+        self.movies = db['movies']
+        self.games = db['games']
+        self.players = db['players']
+        self.current_player = None
+        self.current_game = None
+        self.current_movie = None
+
+    def create_player(self, player_id):
+        player = {
+            'player_id': player_id,
+            'date_created': datetime.utcnow().isoformat(),
+            'games_played': 1,
+            'cookies': []  # TODO: Figure out how cookies work
+        }
+        self.players.insert_one(player)
+        return player
+
+    def get_player(self, player_id: str):
+        """
+        Gets player object from MongoDB OR creates and writes new player to
+        MongoDB and returns that dict
+        :param player_id:
+        :return: dict
+        """
+        cur = self.players.find({'player_id': player_id})
+        docs = [c for c in cur]
+        if len(docs) == 1:
+            return docs[0]
+        elif len(docs) == 0:
+            print(f'New player {player_id} must be created.')
+            return self.create_player(player_id)
+        else:
+            print(f'Something is wrong\ndocs: {docs}')
+            sys.exit(100)
+
+    def create_game(self, game_id: str, ):
+        """
+        creates new game object in
+        :param game_id:
+        :param player_id:
+        :return:
+        """
+        game = {
+            'game_id': game_id,
+            'date_started': datetime.utcnow().isoformat(),
+            'player_id': player_id,
+
+        }
+        self.games.insert_one(game)
+        return game
+
+    def get_game(self, player_id):
+        pass
+
+    def write_guess_to_game(self, guess):
+        pass
+
+    def get_previous_movies(self):
+        pass
+
+
+# Todo:
+#   - add scoring system (Game() stores it, GameHost() changes it)
+#   - - points based on clue count (5 points at clue 1, 1 point at clue 5)
+#   - - Lose 1 point for guessing wrong once each round
+#   - add guessing timer (30 seconds per clue? 15?)
 if __name__ == '__main__':
-    host = GameHost(os.getenv('MONGO_URL'), )
+    host = GameHost(os.getenv('MONGO_URL'), player_id='mgiordano', )
     win = False
     choices = host._get_three_movie_options()
     print(f'Please select a year:')
@@ -97,22 +209,14 @@ if __name__ == '__main__':
     choice: int = int(input('> ')) - 1
     movie = host.get_movie(choices[choice]['_id'])
     host.get_clues_from_movie(movie)
-    ignored_words = ['the', 'a']
     print(f"****ANSWER: '{movie['title']}' {movie['year']}  *****")
     for idx, clue in enumerate(host.rounds[0]['clues']):
         print(f'This warning is in the {clue["category"]} category:\n')
         print(f'For {clue["points"]} points...\n')
         print(clue['text'])
-        true_answer = ''.join(e for e in movie['title'].lower() if e.isalnum())
-        player_answer = ''.join(
-            e for e in input('Guess a movie > ').lower() if e.isalnum())
-        print(f'{true_answer}\n{player_answer}')
-        if player_answer == true_answer:
-            print(f'you win {clue["points"]} points!')
-            win = True
-            break
-        else:
-            print(f'WRONG!')
+        player_guess = input('Guess a movie > ').lower()
+        host.verify_answer(movie, player_guess)
+
     if not win:
         print('You lose, loser!')
     else:
