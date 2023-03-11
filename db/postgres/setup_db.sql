46912ad6732116d5d95370b44946876bf8cecdef
Michael Giordano <mgiord1987@gmail.com>
2023-03-11 18:36:51 -0500

    setup_db.sql SCHEMA update

--- db/postgres/setup_db.sql
+++ db/postgres/setup_db.sql
@@ -8,7 +8,7 @@ CREATE TABLE categories
 
 INSERT INTO categories(short_name, display_name)
 VALUES ('nudity', 'Sex & Nudity'),
-       ('violence', 'Violencfe & Gore'),
+       ('violence', 'Violence & Gore'),
        ('profanity', 'Profanity'),
        ('alcohol', 'Alcohol, Drugs & Smoking'),
        ('frightening', 'Frightening & Intense Scenes');
@@ -18,22 +18,23 @@ DROP TABLE IF EXISTS players CASCADE;
 CREATE TABLE players
 (
     player_id    UUID      DEFAULT gen_random_uuid() UNIQUE PRIMARY KEY,
-    username     VARCHAR(50)  UNIQUE NOT NULL,
-    password     VARCHAR(255) NOT NULL,
+    username     VARCHAR(50) UNIQUE NOT NULL,
+    password     VARCHAR(255)       NOT NULL,
     date_created TIMESTAMP DEFAULT NOW()
 );
 
 DROP TABLE IF EXISTS movies CASCADE;
 CREATE TABLE movies
 (
-    movie_id    UUID      DEFAULT gen_random_uuid() UNIQUE PRIMARY KEY,
-    movie_title VARCHAR(255) NOT NULL,
-    year        INTEGER      NOT NULL,
-    imdb_id     varchar(12) UNIQUE NOT NULL,
-    date_added  TIMESTAMP DEFAULT NOW()
+    movie_id       UUID      DEFAULT gen_random_uuid() UNIQUE PRIMARY KEY,
+    title          TEXT           NOT NULL,
+    stripped_title TEXT           NOT NULL,
+    release_year   INTEGER        NOT NULL,
+    imdb_id        VARCHAR UNIQUE NOT NULL,
+    date_added     TIMESTAMP DEFAULT NOW()
 );
 CREATE UNIQUE INDEX idx_imdb_id ON movies (imdb_id);
-CREATE INDEX idx_title_year ON movies (movie_title, year);
+CREATE INDEX idx_title_year ON movies (stripped_title, release_year);
 
 
 
@@ -45,29 +46,29 @@ CREATE TABLE games
     score      INTEGER   DEFAULT 0     NOT NULL,
     round      INTEGER   DEFAULT 0     NOT NULL,
     start_time TIMESTAMP DEFAULT NOW() NOT NULL,
-    end_time   TIMESTAMP DEFAULT NOW() ON UPDATE NOW()
+    end_time   TIMESTAMP               NULL
 );
-CREATE UNIQUE INDEX idx_game_of_player ON games(game_id, player_id);
+CREATE UNIQUE INDEX idx_game_of_player ON games (game_id, player_id);
 
 DROP TABLE IF EXISTS guesses CASCADE;
 CREATE TABLE guesses
 (
-    guess_id         UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    guess_id         UUID      DEFAULT gen_random_uuid() PRIMARY KEY,
     player_id        UUID REFERENCES players (player_id),
     game_id          UUID REFERENCES games (game_id),
-    guess_text       TEXT      NOT NULL,
+    guess_text       TEXT                    NOT NULL,
     guessed_movie_id UUID REFERENCES movies (movie_id),
     guess_time       TIMESTAMP DEFAULT NOW() NOT NULL,
-    is_correct       BOOLEAN   NOT NULL
+    is_correct       BOOLEAN                 NOT NULL
 );
 
 
 DROP TABLE IF EXISTS scores CASCADE;
 CREATE TABLE scores
 (
-    score_id   UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    score_id   UUID      DEFAULT gen_random_uuid() PRIMARY KEY,
     player_id  UUID REFERENCES players (player_id),
-    score      INTEGER   NOT NULL,
+    score      INTEGER                 NOT NULL,
     score_time TIMESTAMP DEFAULT NOW() NOT NULL
 );
 
@@ -78,6 +79,6 @@ CREATE TABLE clues
     movie_id     UUID REFERENCES movies (movie_id),
     category_id  INT REFERENCES categories (category_id),
     clue_text    TEXT NOT NULL,
-    spoiler      BOOL      DEFAULT FALSE,
+    spoiler      BOOL,
     date_created TIMESTAMP DEFAULT NOW()
 );
